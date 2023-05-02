2c0df9d1644ab6fd399639d0669298140e2000af
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-05-01 21:46:52 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,24 +1,25 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  lazy = true,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  -- lazy = true,
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "auto",
-      },
-      sections = {
-        lualine_x = {
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = { fg = "#ff9e64" },
-          },
-        },
-      },
-    })
-  end,
-}
+return {}
+-- return {
+--   "nvim-lualine/lualine.nvim",
+--   lazy = true,
+--   dependencies = {
+--     "nvim-tree/nvim-web-devicons",
+--   },
+--   -- lazy = true,
+--   config = function()
+--     require("lualine").setup({
+--       options = {
+--         theme = "auto",
+--       },
+--       sections = {
+--         lualine_x = {
+--           {
+--             require("lazy.status").updates,
+--             cond = require("lazy.status").has_updates,
+--             color = { fg = "#ff9e64" },
+--           },
+--         },
+--       },
+--     })
+--   end,
+-- }
