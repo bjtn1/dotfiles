01eea331a56a9781dd30b660f31971d7e173d4c1
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-05-01 21:57:19 -0400

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/neorg.lua

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -1,72 +1,22 @@
--- FIX new neorg breaking changes just dropped 2023-04-20
-local plugin = {
+return {
   "nvim-neorg/neorg",
   build = ":Neorg sync-parsers",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+  dependencies = { { "nvim-lua/plenary.nvim" } },
   opts = {
     load = {
-      ["core.defaults"] = {
-        config = {},
-      },
-      ["core.export"] = {
-        config = {},
-      },
-      ["core.export.markdown"] = {
-        config = {
-          extensions = "all",
-        },
-      },
-      ["core.norg.completion"] = {
-        config = {
-          engine = "nvim-cmp",
-        },
-      },
-      ["core.norg.concealer"] = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {
         config = {
           folds = false,
         },
-      },
-      ----------------------
-      -- Developer modules
-      ----------------------
-      ["core.keybinds"] = {
-        config = {},
-      },
-      ["core.norg.esupports.metagen"] = {
+      },               -- Adds pretty icons to your documents
+      ["core.dirman"] = { -- Manages Neorg workspaces
         config = {
-          type = "auto",
-          template = {
-            { "Author(s)", "Brandon J. T. Noguera" },
-            {
-              "Created",
-              function()
-                return os.date("%Y-%m-%d")
-              end,
-            },
+          workspaces = {
+            notes = "~/notes",
           },
         },
-      },
-      ["core.clipboard"] = {
-        config = {},
-      },
-      ["core.norg.journal"] = {
-        config = {},
-      },
-      ["core.integrations.treesitter"] = {
-        config = {},
-      },
-      ["core.neorgcmd"] = {
-        config = {},
-      },
-      ["core.integrations.nvim-cmp"] = {
-        config = {},
-      },
-      ["core.mode"] = {
-        config = {},
       },
     },
   },
 }
-return {}
