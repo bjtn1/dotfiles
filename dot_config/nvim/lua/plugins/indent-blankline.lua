05d05f5773d84f07e92b443b8d62fa914e952b9b
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-10-05 14:55:26 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/indent-blankline.lua

--- dot_config/nvim/lua/plugins/indent-blankline.lua
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -5,10 +5,10 @@ return {
   config = function()
     require("ibl").setup({
       -- for example, context is off by default, use this to turn it on
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
+      -- char = "│",
+      -- filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+      -- show_trailing_blankline_indent = false,
+      -- show_current_context = false,
     })
   end,
 }
