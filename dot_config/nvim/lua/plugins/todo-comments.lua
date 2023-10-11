65dd945017795d8880860891f64adbb695ec2d24
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-10-11 13:09:43 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/todo-comments.lua

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -5,14 +5,15 @@ return {
     require("todo-comments").setup({
       highlight = {
         multiline_context = 100,
-        -- TODO ooga
-        -- HACK booga
-        -- PERF ooga
-        -- FIX Dick
-        -- WARN ooga
-        -- NOTE booga
-        before = "fg",
-        keyword = "bg",                 -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+        -- ooga TODO booga
+        -- ooga HACK booga
+        -- ooga PERF booga
+        -- ooga FIX booga
+        -- ooga WARN ooga
+        -- ooga NOTE booga
+
+        -- before = "fg",
+        keyword = "bg",             -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
         pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlightng (vim regex)
       },
     })
