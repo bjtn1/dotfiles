c7d72ce98bd74ecd28eabb34e7ef532a68c02719
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-11-07 10:11:51 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorschemes.lua Update .config/nvim/lua/plugins/nvim-autopairs.lua

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -3,74 +3,75 @@ return {
   lazy = false,
   priority = 1000,
   dependencies = {
-    -- "Shatur/neovim-ayu",
-    -- "catppuccin/nvim",
-    -- "Mofiqul/dracula.nvim",
+    "Shatur/neovim-ayu",
+    "catppuccin/nvim",
+    "Mofiqul/dracula.nvim",
     -- NOTE idk why this one aint working "Everblush/nvim",
-    -- "neanias/everforest-nvim",
-    -- "ellisonleao/gruvbox.nvim",
-    -- "lunarvim/horizon.nvim",
-    -- "rebelot/kanagawa.nvim",
-    -- "marko-cerovac/material.nvim",
-    -- "savq/melange-nvim",
-    -- "kdheepak/monochrome.nvim",
-    -- "tanvirtin/monokai.nvim",
-    -- "EdenEast/nightfox.nvim",
-    -- "LunarVim/synthwave84.nvim",
-    -- "decaycs/decay.nvim",
-    -- "shaunsingh/nord.nvim",
-    -- "rose-pine/neovim",
+    "neanias/everforest-nvim",
+    "ellisonleao/gruvbox.nvim",
+    "lunarvim/horizon.nvim",
+    "rebelot/kanagawa.nvim",
+    "marko-cerovac/material.nvim",
+    "savq/melange-nvim",
+    "kdheepak/monochrome.nvim",
+    "tanvirtin/monokai.nvim",
+    "EdenEast/nightfox.nvim",
+    "LunarVim/synthwave84.nvim",
+    "decaycs/decay.nvim",
+    "shaunsingh/nord.nvim",
+    "rose-pine/neovim",
   },
   config = function()
-    -- local ayu = require("ayu")
-    -- local catpuccin = require("catppuccin")
-    -- local dracula = require("dracula")
     -- local everblush = require("everblush")
-    -- local everforest = require("everforest")
-    -- local gruvbox = require("gruvbox")
-    -- local horizon = require("horizon")
-    -- local kanagawa = require("kanagawa")
-    -- local material = require("material")
-    -- local monokai = require("monokai")
-    -- local nightfox = require("nightfox")
-    -- local rose_pine = require("rose-pine")
-    -- local synthwave84 = require("synthwave84")
+    local ayu = require("ayu")
+    local catpuccin = require("catppuccin")
+    local dracula = require("dracula")
+    local everforest = require("everforest")
+    local gruvbox = require("gruvbox")
+    local horizon = require("horizon")
+    local kanagawa = require("kanagawa")
+    local material = require("material")
+    local monokai = require("monokai")
+    local nightfox = require("nightfox")
+    local rose_pine = require("rose-pine")
+    local synthwave84 = require("synthwave84")
     local tokyonight = require("tokyonight")
 
-    -- ayu.setup({})
-    -- catpuccin.setup({
-    --   transparent_background = true,
-    -- })
-    -- dracula.setup({})
-    -- -- everblush.setup({})
-    -- everforest.setup({
-    --   background = "hard",
-    -- })
-    -- gruvbox.setup({})
-    -- horizon.setup({})
-    -- kanagawa.setup({
-    --   transparent = true,
-    -- })
-    -- material.setup({})
-    -- monokai.setup({})
-    -- nightfox.setup({})
-    -- rose_pine.setup({
-    --   disable_background = true,
-    -- })
+    -- everblush.setup({})
+
+    ayu.setup({})
+    catpuccin.setup({
+      transparent_background = true,
+    })
+    everforest.setup({
+    dracula.setup({})
+      background = "hard",
+    })
+    gruvbox.setup({})
+    horizon.setup({})
+    kanagawa.setup({
+      transparent = true,
+    })
+    material.setup({})
+    monokai.setup({})
+    nightfox.setup({})
+    rose_pine.setup({
+      disable_background = true,
+    })
 
     tokyonight.setup({
       transparent = true,
     })
 
-    -- function ColorMyPencils(color)
-    -- 	color = color or "rose-pine"
-    -- 	vim.cmd.colorscheme(color)
-    --
-    -- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    -- end
-    --
-    -- ColorMyPencils()
-    -- synthwave84.setup({})
+    function ColorMyPencils(color)
+    	color = color or "rose-pine"
+    	vim.cmd.colorscheme(color)
+
+    	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
+
+    ColorMyPencils()
+    synthwave84.setup({})
   end,
 }
