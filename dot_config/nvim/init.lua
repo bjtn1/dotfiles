952ab1393c3a7edb03483a7ffdebda6c86058846
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-11-07 16:50:34 -0500

    Update .config/nvim/init.lua Add .config/nvim/lua/autocommands.lua Update .config/nvim/lua/options.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -6,7 +6,7 @@
 -- HACK
 
 -- This disables netrw
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -24,10 +24,11 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
+local color = "tokyonight-night"
 
 local opts = {
   install = {
-    colorscheme = { "tokyonight-night" },
+    colorscheme = { color },
   },
   ui = {
     border = "single",
@@ -40,41 +41,6 @@ local opts = {
 require("options")
 require("lazy").setup("plugins", opts)
 require("keymaps")
-
-local color = "tokyonight-night"
+require("autocommands")
 
 vim.cmd.colorscheme(color)
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-vim.opt.fillchars:append("eob: ")
-
-
-local group = vim.api.nvim_create_augroup(
-  "AutoInstallNewLSPs",
-  {
-    clear = true
-  }
-)
-vim.api.nvim_create_autocmd(
-  "BufRead",
-  {
-    callback = function()
-      -- Get file path of current buffer
-      -- TODO find the correct evet so that the autocommand gets called anytipe we open a file
-      local file_path = vim.api.nvim_buf_get_name(0)
-      local file = io.open(file_path, "r")
-      if file~=nil then
-        print(file_path .. " is a file")
-        -- TODO figure out if theres an LSP attached to this buffer
-        io.close(file)
-        return true
-      else
-        return false
-      end
-
-    end,
-    group = group,
-  }
-)
