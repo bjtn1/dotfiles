7d8fcf45cd0e34aefa36c963435a428bf3e86f0f
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-12-06 13:36:43 -0500

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/latex.lua Update .config/nvim/lua/plugins/lsp.lua Remove .config/nvim/lua/snippets.FIX/all.lua Remove .config/nvim/lua/snippets.lua Add .config/nvim/snippets/all.lua Add .config/nvim/snippets/tex/greek.lua

--- dot_config/nvim/lua/plugins/latex.lua
+++ dot_config/nvim/lua/plugins/latex.lua
@@ -11,13 +11,37 @@ return {
   config = function ()
     vim.g["vimtex_quickfix_open_on_warning"] = 0
     vim.g["vimtex_view_method"] = "skim"
+    vim.cmd[[
+    let g:vimtex_syntax_packages = {'minted': {'load': 2}}
+    ]]
+    vim.cmd[[
+      let g:vimtex_quickfix_ignore_filters = [
+      \ 'Underfull \\hbox',
+      \ 'Overfull \\hbox',
+      \ 'LaTeX Warning: .\+ float specifier changed to',
+      \ 'LaTeX hooks Warning',
+      \ 'Package siunitx Warning: Detected the "physics" package:',
+      \ 'Package hyperref Warning: Token not allowed in a PDF string',
+      \]
+      ]]
+    vim.cmd[[
+    let g:vimtex_compiler_latexmk = {
+    \ 'options' : [
+    \   '-shell-escape',
+    \   '-verbose',
+    \   '-file-line-error',
+    \   '-synctex=1',
+    \   '-interaction=nonstopmode',
+    \ ],
+    \}
+    ]]
     -- From:
     -- https://www.ejmastnak.com/tutorials/vim-latex/luasnip/#keymaps
     vim.cmd[[
     " Jump forward through tabstops in insert and visual mode with control+j or jk
     imap <silent><expr> jk luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<C-j>'
     smap <silent><expr> jk luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<C-j>'
-    
+
     " Jump backward through snippet tabstops with kj or Shift-Tab 
     imap <silent><expr> kj luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
     smap <silent><expr> kj luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
