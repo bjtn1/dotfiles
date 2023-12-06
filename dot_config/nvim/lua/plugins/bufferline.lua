ad447a3b1d69d11b7854c312dd0b411c18d60461
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-12-06 03:59:01 -0500

    Update .config/nvim/lua/plugins/bufferline.lua

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -10,7 +10,21 @@ return {
         indicator = {
           style = "icon",
         },
+        numbers = "ordinal",
       },
     })
+
+    vim.cmd[[
+      nnoremap <silent><leader>1 <cmd>lua require("bufferline").go_to(1, true)<cr>
+      nnoremap <silent><leader>2 <cmd>lua require("bufferline").go_to(2, true)<cr>
+      nnoremap <silent><leader>3 <cmd>lua require("bufferline").go_to(3, true)<cr>
+      nnoremap <silent><leader>4 <cmd>lua require("bufferline").go_to(4, true)<cr>
+      nnoremap <silent><leader>5 <cmd>lua require("bufferline").go_to(5, true)<cr>
+      nnoremap <silent><leader>6 <cmd>lua require("bufferline").go_to(6, true)<cr>
+      nnoremap <silent><leader>7 <cmd>lua require("bufferline").go_to(7, true)<cr>
+      nnoremap <silent><leader>8 <cmd>lua require("bufferline").go_to(8, true)<cr>
+      nnoremap <silent><leader>9 <cmd>lua require("bufferline").go_to(9, true)<cr>
+      nnoremap <silent><leader>$ <cmd>lua require("bufferline").go_to(-1, true)<cr>
+      ]]
   end,
 }
