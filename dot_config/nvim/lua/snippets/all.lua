e172d25e4435c79bf7482922a2d9aea309c6b20d
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-12-06 01:58:46 -0500

    Update .config/nvim/filetype.vim Update .config/nvim/lazy-lock.json Update .config/nvim/lua/keymaps.lua Update .config/nvim/lua/options.lua Update .config/nvim/lua/plugins/latex.lua Update .config/nvim/lua/plugins/nvim-autopairs.lua Update .config/nvim/lua/plugins/nvim-treesitter.lua Update .config/nvim/lua/snippets/all.lua

--- dot_config/nvim/lua/snippets/all.lua
+++ dot_config/nvim/lua/snippets/all.lua
@@ -1,3 +1,7 @@
+-- " You're gonna ask where this came from....
+-- " It comes from here:
+-- " https://www.ejmastnak.com/tutorials/vim-latex/vimtex/
+-- " You added this because you wanted to try LaTeX for math on nvim
 return {
   -- A snippet that expands the trigger "hi" into the string "Hello, world!".
   require("luasnip").snippet(
