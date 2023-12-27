2d90ee665da000c74be451a28633e481e20014b0
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-12-27 13:10:20 -0500

    Update .config/nvim/lazy-lock.json Remove .config/nvim/lua/snippets/tex/env-align.lua Remove .config/nvim/lua/snippets/tex/env-enumerate.lua Remove .config/nvim/lua/snippets/tex/env-equation.lua Remove .config/nvim/lua/snippets/tex/env-figure.lua Remove .config/nvim/lua/snippets/tex/env-gather.lua Remove .config/nvim/lua/snippets/tex/env-general.lua Remove .config/nvim/lua/snippets/tex/env-itemize.lua Remove .config/nvim/lua/snippets/tex/env-piecewise-function.lua Remove .config/nvim/lua/snippets/tex/env-split.lua Remove .config/nvim/lua/snippets/tex/env-table.lua Remove .config/nvim/lua/snippets/tex/env-tcolorbox.lua Remove .config/nvim/lua/snippets/tex/env-verbatim.lua Update .config/nvim/lua/snippets/tex/environments.lua Change attributes of .config/nvim/lua/snippets/tex/tikzpicture.lua

--- dot_config/nvim/lua/snippets/tex/environments.lua
+++ dot_config/nvim/lua/snippets/tex/environments.lua
@@ -3,6 +3,124 @@ local u = require("helpers.tex.utils")
 
 return
   {
+    ------------------------------ BEGIN TCOLORBOX SECTION ------------------------------
+    s(
+      {
+        trig="tcb",
+        snippetType="autosnippet",
+      },
+      fmta(
+        [[
+        \begin{tcolorbox}[title=<>]
+        <>
+        \end{tcolorbox}<>
+        ]],
+        {
+          i(1),
+          i(2),
+          i(0),
+        }
+      ),
+      {condition=u.line_begin}
+    ),
+    ------------------------------ END TCOLORBOX SECTION ------------------------------
+
+    ------------------------------ BEGIN VERBATIM SECTION ------------------------------
+    s(
+      {
+        trig="vrb",
+        snippetType="autosnippet",
+      },
+      fmta(
+        [[
+        \begin{Verbatim}[numbers=left, frame=single]
+        <>
+        \end{Verbatim}<>
+        ]],
+        {
+          i(1),
+          i(0),
+        }
+      ),
+      {condition=u.line_begin}
+    ),
+    ------------------------------ END VERBATIM SECTION ------------------------------
+
+    ------------------------------ BEGIN ITEMIZE SECTION ------------------------------
+    -- For adding a new item to an itemize section
+    s(
+      {
+        trig="-",
+        dscr="Itemize environment",
+        snippetType="autosnippet"
+      },
+      fmta(
+        [[
+        \begin{itemize}
+            \item <>
+        \end{itemize}
+        ]],
+        {
+          i(1),
+        }
+      ),
+      {
+        condition=u.line_begin and u.in_text
+      }
+    ),
+
+    s(
+      {
+        trig="ii",
+        snippetType="autosnippet"
+      },
+      fmta(
+        [[
+        \item <>
+        <>
+        ]],
+        {
+          i(1),
+          i(0),
+        }
+      ),
+      {
+        condition=u.line_begin and u.in_text and u.in_itemize
+      }
+    ),
+
+    ------------------------------ END ITEMIZE SECTION ------------------------------
+
+    ------------------------------ BEGIN FIGURE SECTION ------------------------------
+    -- For adding figures
+    s(
+      {
+        trig="fig",
+        dscr="Figure environment",
+        snippetType="autosnippet",
+      },
+      fmta(
+        [[
+        \begin{figure}[htb!]
+          \centering
+          \includegraphics[width=<>\linewidth]{<>}
+          \caption{<>}
+          \label{fig:<>}
+        \end{figure}
+        ]],
+        {
+          i(1),
+          i(2),
+          i(3),
+          i(4),
+        }
+      ),
+      {
+        condition=u.line_begin
+      }
+    ),
+    ------------------------------ END FIGURE SECTION ------------------------------
+
     ------------------------------ BEGIN ALIGN SECTION ------------------------------
     -- For adding intertext in an align environment
     s(
