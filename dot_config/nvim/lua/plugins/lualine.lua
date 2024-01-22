d93798824bead13d79d46d89fc8b9df2f692161e
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2024-01-22 14:29:42 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -21,27 +21,22 @@ return
         green  = "#c3e88d",
       }
 
-      local bubbles_theme = {
+      local theme = {
         normal = {
           a = { fg = colors.black, bg = colors.blue },
-          b = { fg = colors.white, bg = colors.grey },
-          c = { fg = colors.black, bg = colors.black },
+          b = { fg = colors.blue, bg = colors.grey },
+          c = { fg = colors.green, bg = colors.black },
+          z = { fg = colors.green, bg = colors.grey },
         },
 
         insert = { a = { fg = colors.black, bg = colors.green } },
         visual = { a = { fg = colors.black, bg = colors.violet } },
         replace = { a = { fg = colors.black, bg = colors.red } },
-
-        inactive = {
-          a = { fg = colors.white, bg = colors.black },
-          b = { fg = colors.white, bg = colors.black },
-          c = { fg = colors.black, bg = colors.black },
-        },
       }
 
       require('lualine').setup {
         options = {
-          theme = bubbles_theme,
+          theme = theme,
           component_separators = '|',
           section_separators = { left = '', right = '' },
         },
@@ -49,24 +44,12 @@ return
           lualine_a = {
             { 'mode', separator = { left = '' }, right_padding = 2 },
           },
-          lualine_b = { 'filename', 'branch' },
-          lualine_c = { 'fileformat' },
-          lualine_x = {},
-          lualine_y = { 'filetype', 'progress' },
-          lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 2 },
-          },
-        },
-        inactive_sections = {
-          lualine_a = { 'filename' },
-          lualine_b = {},
-          lualine_c = {},
+          lualine_b = { 'filename' },
+          lualine_c = { "branch" },
           lualine_x = {},
           lualine_y = {},
-          lualine_z = { 'location' },
+          lualine_z = {},
         },
-        tabline = {},
-        extensions = {},
       }
     end
   }
