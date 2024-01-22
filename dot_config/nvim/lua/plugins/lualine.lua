e4313d41af0f1ab53f5c97ec99aabf4e7f6d4d6a
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2024-01-22 13:46:12 -0500

    Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -5,17 +5,68 @@ return
       "nvim-tree/nvim-web-devicons"
     },
     config = function ()
-      local ll = require("lualine")
-      ll.setup({
+      -- Bubbles config for lualine
+      -- Author: lokesh-krishna
+      -- MIT license, see LICENSE for more details.
+
+      -- stylua: ignore
+      local colors = {
+        blue   = '#82aaff',
+        cyan   = '#86e1fc',
+        black  = '#1a1b26',
+        white  = '#c8d3f5',
+        red    = '#f38ba8',
+        violet = '#c099ff',
+        grey   = '#313244',
+        green  = "#c3e88d",
+      }
+
+      local bubbles_theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.blue },
+          b = { fg = colors.white, bg = colors.grey },
+          c = { fg = colors.black, bg = colors.black },
+        },
+
+        insert = { a = { fg = colors.black, bg = colors.green } },
+        visual = { a = { fg = colors.black, bg = colors.violet } },
+        replace = { a = { fg = colors.black, bg = colors.red } },
+
+        inactive = {
+          a = { fg = colors.white, bg = colors.black },
+          b = { fg = colors.white, bg = colors.black },
+          c = { fg = colors.black, bg = colors.black },
+        },
+      }
+
+      require('lualine').setup {
+        options = {
+          theme = bubbles_theme,
+          component_separators = '|',
+          section_separators = { left = '', right = '' },
+        },
         sections = {
-          lualine_x = {
-            {
-              require("noice").api.statusline.mode.get,
-              cond = require("noice").api.statusline.mode.has,
-              color = { fg = "#ff9e64" },
-            }
+          lualine_a = {
+            { 'mode', separator = { left = '' }, right_padding = 2 },
+          },
+          lualine_b = { 'filename', 'branch' },
+          lualine_c = { 'fileformat' },
+          lualine_x = {},
+          lualine_y = { 'filetype', 'progress' },
+          lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
           },
         },
-      })
+        inactive_sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+        tabline = {},
+        extensions = {},
+      }
     end
   }
