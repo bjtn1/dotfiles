72eb2f3c95e00c1462b65b4e47b6b36d80d2b3cc
Brandon J. T. Noguera <software_dev.snore288@slmail.me>
2024-04-21 00:43:19 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/keymaps.lua Update .config/nvim/lua/options.lua Update .config/nvim/lua/plugins/neorg.lua Update .config/nvim/lua/plugins/noice.lua Update .config/nvim/lua/plugins/todo-comments.lua Update .config/nvim/lua/plugins/vim-tmux-navigator.lua Update .config/nvim/lua/plugins/which-key.lua

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,16 +1,15 @@
-return {
-  "folke/which-key.nvim",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup({
-      window = {
-        border = "single"
-      },
-      layout = {
-        spacing = 10,
-        align = "left",
-      }
-    })
-  end
-}
+return
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup({
+        window = {
+          border = "single"
+        },
+        layout = {
+          spacing = 10,
+          align = "left",
+        }
+      })
+    end
+  }
