1bd78f337e974a0040a82cf6d7b0d156175e2491
Brandon J. T. Noguera <software_dev.snore288@slmail.me>
2024-04-21 17:04:22 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/keymaps.lua Update .config/nvim/lua/plugins/lsp.lua Update .config/nvim/lua/plugins/lualine.lua Update .config/nvim/lua/plugins/neo-tree.lua Update .config/nvim/lua/plugins/noice.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -35,29 +35,94 @@ wk.register({
     c = { "<Plug>Csurround", "Change<old_delimiter><new_delimiter>" },
     A = { "<Plug>Yssurround", "Add in line" },
   },
-  ["<leader>s"] = {
+
+  ["<leader>ss"] = {
     name = "+Screenshot",
     f = { "<cmd>Silicon<cr>", "File" },
   },
+
   ["<leader>b"] = {
     name = "+Buffers",
-    a = { '<cmd> lua require("bufferline").go_to(1, true)<cr>', "Go to buffer 1"},
-    b = { '<cmd> lua require("bufferline").go_to(2, true)<cr>', "Go to buffer 2"},
-    c = { '<cmd> lua require("bufferline").go_to(3, true)<cr>', "Go to buffer 3"},
-    d = { '<cmd> lua require("bufferline").go_to(4, true)<cr>', "Go to buffer 4"},
-    e = { '<cmd> lua require("bufferline").go_to(5, true)<cr>', "Go to buffer 5"},
-    f = { '<cmd> lua require("bufferline").go_to(6, true)<cr>', "Go to buffer 6"},
-    g = { '<cmd> lua require("bufferline").go_to(7, true)<cr>', "Go to buffer 7"},
-    h = { '<cmd> lua require("bufferline").go_to(8, true)<cr>', "Go to buffer 8"},
-    i = { '<cmd> lua require("bufferline").go_to(9, true)<cr>', "Go to buffer 9"},
-    j = { '<cmd> lua require("bufferline").go_to(-1, true)<cr>', "Go to last buffer"},
+    j = { "<cmd>BufferLinePick<cr>", "Jump" },
+    f = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+    b = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
+    n = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+    W = { "<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)" },
+    e = { "<cmd>BufferLinePickClose<cr>", "Pick which buffer to close" },
+    h = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
+    l = { "<cmd>BufferLineCloseRight<cr>", "Close all to the right" },
+    D = { "<cmd>BufferLineSortByDirectory<cr>", "Sort by directory" },
+    L = { "<cmd>BufferLineSortByExtension<cr>", "Sort by language" },
   },
+
+  ["<leader>1"] = { '<cmd> lua require("bufferline").go_to(1, true)<cr>', "Go to buffer 1"},
+  ["<leader>2"] = { '<cmd> lua require("bufferline").go_to(2, true)<cr>', "Go to buffer 2"},
+  ["<leader>3"] = { '<cmd> lua require("bufferline").go_to(3, true)<cr>', "Go to buffer 3"},
+  ["<leader>4"] = { '<cmd> lua require("bufferline").go_to(4, true)<cr>', "Go to buffer 4"},
+  ["<leader>5"] = { '<cmd> lua require("bufferline").go_to(5, true)<cr>', "Go to buffer 5"},
+  ["<leader>6"] = { '<cmd> lua require("bufferline").go_to(6, true)<cr>', "Go to buffer 6"},
+  ["<leader>7"] = { '<cmd> lua require("bufferline").go_to(7, true)<cr>', "Go to buffer 7"},
+  ["<leader>8"] = { '<cmd> lua require("bufferline").go_to(8, true)<cr>', "Go to buffer 8"},
+  ["<leader>9"] = { '<cmd> lua require("bufferline").go_to(9, true)<cr>', "Go to buffer 9"},
+  ["<leader>0"] = { '<cmd> lua require("bufferline").go_to(-1, true)<cr>', "Go to last buffer"},
+
   ["<leader>d"] = {
     name = "+Diagnostics",
     a = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Diagnostics from workspace" },
     c = { "<cmd>TroubleToggle document_diagnostics<cr>", "Diagnostics from current buffer" },
     t = { "<cmd>echo 'LOL THIS DONT WORK YET, ILL FIGURE IT OUT LATER'<cr>", "View TODOs in current directory" },
   },
+
+  ["<leader>g"] = {
+    name = "+Git",
+    g = { "<cmd>lua _Lazygit_toggle()<CR>", "LazyGit" },
+    j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
+    k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
+    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+    s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+    u = { "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "Undo Stage Hunk" },
+    o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+    C = { "<cmd>Telescope git_bcommits<cr>", "Checkout commit(for current file)" },
+    d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Git Diff" },
+  },
+
+  ["<leader>l"] = {
+    name = "+LSP",
+    -- Available keybinds
+    -- vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    -- vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+    -- vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+    -- vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+    -- vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+    -- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+    -- vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+    -- vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    -- vim.keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+    -- vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    -- vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+    -- vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+    -- vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+    w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+    f = { "<cmd>lua require('lvim.lsp.utils').format()<cr>", "Format" },
+    i = { "<cmd>LspInfo<cr>", "Info" },
+    I = { "<cmd>Mason<cr>", "Mason Info" },
+    j = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
+    k = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
+    l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+    q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+    r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+    S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
+    e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+  },
+
   ["<leader>n"] = {
     name = "+Neorg",
     e = { "<cmd>Neorg export to-file<cr>", "Export current .norg to .md" },
@@ -65,6 +130,7 @@ wk.register({
     t = { "<cmd>Neorg toggle-concealer<cr>", "Toggle concealer" },
     u = { "<cmd>Neorg update-metadata<cr>", "Update metadata" },
   },
+
   ["<leader>o"] = {
     name = "+Open",
     g = { "<cmd>lua _Lazygit_toggle()<CR>", "LazyGit" },
@@ -72,11 +138,45 @@ wk.register({
     m = { "<cmd>Mason<cr>", "Mason" },
     w = {"<cmd>WhichKey<cr>", "WhichKey"},
   },
+
+  ["<leader>p"] = {
+    name = "+Plugins",
+    i = { "<cmd>Lazy install<cr>", "Install" },
+    s = { "<cmd>Lazy sync<cr>", "Sync" },
+    S = { "<cmd>Lazy clear<cr>", "Status" },
+    c = { "<cmd>Lazy clean<cr>", "Clean" },
+    u = { "<cmd>Lazy update<cr>", "Update" },
+    p = { "<cmd>Lazy profile<cr>", "Profile" },
+    l = { "<cmd>Lazy log<cr>", "Log" },
+    d = { "<cmd>Lazy debug<cr>", "Debug" },
+  },
+
   ["<leader>R"] = {
     name = "+Restore",
     c = { '<cmd>lua require("persistence").load()<cr>', "Current directory's last session" },
     l = { '<cmd>lua require("persistence").load({last = true})<cr>', "Last session" },
   },
+
+  ["<leader>s"] = {
+    name = "+Search",
+    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+    f = { "<cmd>Telescope find_files<cr>", "Find File" },
+    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+    H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+    R = { "<cmd>Telescope registers<cr>", "Registers" },
+    t = { "<cmd>Telescope live_grep<cr>", "Text" },
+    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+    C = { "<cmd>Telescope commands<cr>", "Commands" },
+    l = { "<cmd>Telescope resume<cr>", "Resume last search" },
+    p = {
+      "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+      "Colorscheme with Preview",
+    },
+  },
+
   ["<leader>t"] = {
     name = "+Telescope",
     f = { "<cmd>Telescope find_files<cr>", "Find files" },
@@ -87,6 +187,7 @@ wk.register({
     r = { ":%s/", "Search and replace"},
     k = { "<cmd> Telescope keymaps<cr>", "See keymaps" },
   },
+
   ["<leader>v"] = {
     name = "+VimTex",
     i = {"<cmd>VimtexInfo<cr>", "VimTex Info"},
@@ -96,6 +197,7 @@ wk.register({
     C = {"<cmd>VimtexClean<cr>", "Clean aux files"},
     r = {"<cmd>lua require('luasnip.loaders.from_lua').load({paths = '~/.config/nvim/lua/snippets/'})<cr>", "Reload snippets"},
   },
+
   ["<leader>w"] = {
     name = "+Window",
     v = { "<C-w>v", "Split vertically" },
--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,27 +1,28 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function ()
-    local neotree = require("neo-tree")
+return
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function ()
+      local neotree = require("neo-tree")
 
-    neotree.setup({
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_hidden = false,
+      neotree.setup({
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+          },
+          follow_current_file = {
+            enabled = true,
+          },
         },
-        follow_current_file = {
-          enabled = true,
-        },
-      },
-    })
-  end
-}
+      })
+    end
+  }
