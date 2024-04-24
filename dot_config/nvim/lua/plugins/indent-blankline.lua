74031952c38a9035e105336cf62a19b4c8c1ecd0
Brandon J. T. Noguera <software_dev.snore288@slmail.me>
2024-04-24 04:28:40 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/keymaps.lua Update .config/nvim/lua/plugins/indent-blankline.lua Remove .config/nvim/lua/plugins/luarocks.lua Update .config/nvim/lua/plugins/neo-tree.lua Remove .config/nvim/lua/plugins/neorg.lua Remove .config/nvim/lua/plugins/nvim-tree.lua Remove .config/nvim/lua/plugins/presence.lua Update .config/nvim/lua/plugins/todo-comments.lua

--- dot_config/nvim/lua/plugins/indent-blankline.lua
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -6,7 +6,7 @@ return {
     ibl.setup({
       -- for example, context is off by default, use this to turn it on
       -- char = "│",
-      -- filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+      -- filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "terminal" },
       -- show_trailing_blankline_indent = false,
       -- show_current_context = false,
     })
