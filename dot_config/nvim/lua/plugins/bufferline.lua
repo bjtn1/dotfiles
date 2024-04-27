7dd8b3e48a64e7af20c2d727e97e5f06d0c1897c
Brandon J. T. Noguera <software_dev.snore288@slmail.me>
2024-04-27 03:40:46 -0400

    Update .config/nvim/lua/.luarc.json Update .config/nvim/lua/plugins/bufferline.lua Update .config/nvim/lua/plugins/lsp.lua Update .config/nvim/lua/plugins/noice.lua Update .config/nvim/lua/plugins/nvim-treesitter.lua

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -9,6 +9,16 @@ return
       require("bufferline").setup({
         -- highlights = require("catppuccin.groups.integrations.bufferline").get(),
         options = {
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and "  "
+              or (e == "warning" and "  " or "  " )
+              s = s .. n .. sym
+            end
+            return s
+          end,
           indicator = {
             style = "icon",
           },
--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -14,6 +14,7 @@ return {
         "html",
         "java",
         "javascript",
+        "json",
         "latex",
         "lua",
         "markdown",
