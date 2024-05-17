1b9f42a75b75f5e7c0eb08551136b67592548a4c
Brandon J. T. Noguera <software_dev.snore288@slmail.me>
2024-05-17 13:05:13 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/neorg.lua

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,37 +1,26 @@
-return
-  {
-    "akinsho/bufferline.nvim",
-    version = "v3.*",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("bufferline").setup({
-        -- highlights = require("catppuccin.groups.integrations.bufferline").get(),
-        options = {
-          diagnostics = "nvim_lsp",
-          diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            local s = " "
-            for e, n in pairs(diagnostics_dict) do
-              local sym = e == "error" and " e "
-              or (e == "warning" and " w " or " h " )
-              s = s .. n .. sym
-            end
-            return s
-          end,
-          indicator = {
-            style = "icon",
-          },
-          numbers = "ordinal",
-          offsets = {
-            {
-              filetype = "NeoTree",
-              text = "File Explorer",
-              text_align = "left", -- | "center" | "right"
-              separator = true
-            }
+return {
+  "akinsho/bufferline.nvim",
+  -- version = "v3.*",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("bufferline").setup({
+      -- highlights = require("catppuccin.groups.integrations.bufferline").get(),
+      options = {
+        indicator = {
+          style = "icon",
+        },
+        numbers = "ordinal",
+        offsets = {
+          {
+            filetype = "NeoTree",
+            text = "File Explorer",
+            text_align = "left", -- | "center" | "right"
+            separator = true,
           },
         },
-      })
-    end,
-  }
+      },
+    })
+  end,
+}
