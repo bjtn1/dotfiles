81f46ef24eb43e371eae6574c419bfc8c93bdc4b
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2025-05-29 12:18:45 -0400

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -210,7 +210,7 @@ function update
         echo "Running brew reinstall python-gdbm..."
         brew reinstall python-gdbm --verbose
         echo "Running brew reinstall librewolf --no-quarantine..."
-        brew reinstall librewolf --no-quarantine --verbose
+        brew reinstall librewolf --no-quarantine --verbose -f
         echo "Running rustup update..."
         rustup update
         echo "Running brew cleanup..."
