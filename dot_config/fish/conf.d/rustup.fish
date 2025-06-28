2de9e6397011c934287f19960cf3789406a4df7d
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2025-06-28 14:48:21 -0400

    Update .config/fish/conf.d/rustup.fish

--- dot_config/fish/conf.d/rustup.fish
+++ dot_config/fish/conf.d/rustup.fish
@@ -1,3 +1,4 @@
-if command rustup -v > /dev/null
+# only run this source script if this damn file exists (really annnoying on eos)
+if test -f ~/.cargo/env.fish
   source "$HOME/.cargo/env.fish"
 end
