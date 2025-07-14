3f8b0a44d62edbae975271d90d15bc89ca3a8565
bjtn <github@bjtn.me>
2025-07-13 21:47:13 -0400

    Add .config/nvim/filetype.vim Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/autocommands.lua Add .config/nvim/lua/.luarc.json Add .config/nvim/lua/.luarc.json.old Add .config/nvim/lua/helpers/tex/utils.lua Add .config/nvim/lua/keymaps.lua Add .config/nvim/lua/options.lua Add .config/nvim/lua/plugins/Comment.lua Add .config/nvim/lua/plugins/alpha.lua Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/colorschemes.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/indent-blankline.lua Add .config/nvim/lua/plugins/latex.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/lualine.lua Add .config/nvim/lua/plugins/maximizer.lua Add .config/nvim/lua/plugins/md-preview.lua Add .config/nvim/lua/plugins/mini-bufremove.lua Add .config/nvim/lua/plugins/mini-indentscope.lua Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/neorg.lua Add .config/nvim/lua/plugins/neoscroll.lua Add .config/nvim/lua/plugins/noice.lua Add .config/nvim/lua/plugins/nvim-autopairs.lua Add .config/nvim/lua/plugins/nvim-colorizer.lua Add .config/nvim/lua/plugins/nvim-silicon.lua Add .config/nvim/lua/plugins/nvim-toc.lua Add .config/nvim/lua/plugins/nvim-treesitter.lua Add .config/nvim/lua/plugins/nvim-ts-autotag.lua Add .config/nvim/lua/plugins/persistence.lua Add .config/nvim/lua/plugins/telescope-fzf-native.lua Add .config/nvim/lua/plugins/telescope.lua Add .config/nvim/lua/plugins/todo-comments.lua Add .config/nvim/lua/plugins/toggleterm.lua Add .config/nvim/lua/plugins/trouble.lua Add .config/nvim/lua/plugins/vim-markdown-toc.lua Add .config/nvim/lua/plugins/vim-surround.lua Add .config/nvim/lua/plugins/vim-tmux-navigator.lua Add .config/nvim/lua/plugins/which-key.lua Add .config/nvim/lua/snippets/all.lua Add .config/nvim/lua/snippets/md.lua Add .config/nvim/lua/snippets/tex/delimiters.lua Add .config/nvim/lua/snippets/tex/env-inline-math.lua Add .config/nvim/lua/snippets/tex/environments.lua Add .config/nvim/lua/snippets/tex/general.lua Add .config/nvim/lua/snippets/tex/greek.lua Add .config/nvim/lua/snippets/tex/math.lua Add .config/nvim/lua/snippets/tex/templates.lua Add .config/nvim/lua/snippets/tex/tikzpicture.lua

---
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,52 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  priority = 1000,
+  version = false,
+  build = ":TSUpdate",
+  config = function ()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "astro",
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "latex",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        -- "norg",
+        -- "norg_meta",
+        "python",
+        "regex",
+        "rust",
+        "typescript",
+        "toml",
+        "vim"
+      },
+      highlight = {
+        enable = true,
+        -- These two lines com from:
+        -- https://github.com/lervag/vimtex/issues/2469
+        disable = {
+          "latex",
+        },
+        additional_vim_regex_highlighting = { "latex", "markdown" },
+      },
+      indent = {
+        enable = true,
+        disable = "python",
+      },
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = true,
+      },
+    auto_install = true,
+    })
+  end
+}
---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = '0.1.x',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    "sharkdp/fd",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+    -- not maintained at all
+    -- "ahmedkhalf/project.nvim",
+  },
+  config = function ()
+    local actions = require("telescope.actions")
+    local telescope = require("telescope")
+    -- local telescope_project = require("project_nvim")
+
+    -- telescope_project.setup {
+    --   -- your configuration comes here
+    --   -- or leave it empty to use the default settings
+    --   -- refer to the configuration section below
+    -- }
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<esc>"] = actions.close,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          }
+        }
+      },
+      pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
+      },
+    })
+    -- require('telescope').load_extension('projects')
+  end
+}
