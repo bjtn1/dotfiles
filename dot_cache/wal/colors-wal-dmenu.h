43a2e72a51c1d514a358057a8a6bf6b609579d1f
bjtn <github@bjtn.me>
2025-07-18 19:45:07 -0400

    Update .cache/wal/Colors.hs Update .cache/wal/colors Update .cache/wal/colors--nodim-alacritty.yml Update .cache/wal/colors-alacritty.toml Update .cache/wal/colors-alacritty.yml Update .cache/wal/colors-foot.ini Update .cache/wal/colors-fuzzel.ini Update .cache/wal/colors-hyprland.conf Update .cache/wal/colors-kitty.conf Update .cache/wal/colors-konsole.colorscheme Update .cache/wal/colors-leftwm-theme.ron Update .cache/wal/colors-mako Update .cache/wal/colors-nqq.css Update .cache/wal/colors-oomox Update .cache/wal/colors-polybar Update .cache/wal/colors-putty.reg Update .cache/wal/colors-retroarch.cfg Update .cache/wal/colors-rgb Update .cache/wal/colors-rio.toml Update .cache/wal/colors-rofi-dark.rasi Update .cache/wal/colors-rofi-light.rasi Update .cache/wal/colors-speedcrunch.json Update .cache/wal/colors-sway Update .cache/wal/colors-themer.js Update .cache/wal/colors-tilix.json Update .cache/wal/colors-tofi Update .cache/wal/colors-tty.sh Update .cache/wal/colors-vscode.json Update .cache/wal/colors-wal-dmenu.h Update .cache/wal/colors-wal-dwl.h Update .cache/wal/colors-wal-dwm-urg.h Update .cache/wal/colors-wal-dwm.h Update .cache/wal/colors-wal-st.h Update .cache/wal/colors-wal-tabbed.h Update .cache/wal/colors-wal.vim Update .cache/wal/colors-waybar.css Update .cache/wal/colors-zathura Update .cache/wal/colors-zed.json Update .cache/wal/colors.Xresources Update .cache/wal/colors.css Update .cache/wal/colors.fish Update .cache/wal/colors.json Update .cache/wal/colors.properties Update .cache/wal/colors.scss Update .cache/wal/colors.sh Update .cache/wal/colors.styl Update .cache/wal/colors.yml Update .cache/wal/ghostty.conf Update .cache/wal/last_used_theme Add .cache/wal/schemes/_home_bjtn__config_wallpapers_003-wallpaper-4816-1920x1080-3098_jpg_dark_None_None_1125802_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_ratceht-and-clank-quest-for-booty003-wallpaper-5175-1920x1080-3444_jpg_dark_None_None_1421492_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_ratchet-and-clank-quest-for-booty-001-wallpaper-5173-1920x1080-3442_jpg_dark_None_None_1457278_2.0.0.json Update .cache/wal/sequences Update .cache/wal/wal

--- dot_cache/wal/colors-wal-dmenu.h
+++ dot_cache/wal/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c3c4c8", "#000000" },
-	[SchemeSel] = { "#c3c4c8", "#296993" },
-	[SchemeOut] = { "#c3c4c8", "#37A2CB" },
+	[SchemeNorm] = { "#c2c7c4", "#000000" },
+	[SchemeSel] = { "#c2c7c4", "#9EB85C" },
+	[SchemeOut] = { "#c2c7c4", "#6DC0F6" },
 };
