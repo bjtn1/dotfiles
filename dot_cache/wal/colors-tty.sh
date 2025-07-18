26c5b02bda69ce7f6760b39d9bdb3a90bb135d96
bjtn <github@bjtn.me>
2025-07-18 17:38:10 -0400

    Update .cache/wal/Colors.hs Update .cache/wal/colors Update .cache/wal/colors--nodim-alacritty.yml Update .cache/wal/colors-alacritty.toml Update .cache/wal/colors-alacritty.yml Update .cache/wal/colors-foot.ini Update .cache/wal/colors-fuzzel.ini Update .cache/wal/colors-hyprland.conf Update .cache/wal/colors-kitty.conf Update .cache/wal/colors-konsole.colorscheme Update .cache/wal/colors-leftwm-theme.ron Update .cache/wal/colors-mako Update .cache/wal/colors-nqq.css Update .cache/wal/colors-oomox Update .cache/wal/colors-polybar Update .cache/wal/colors-putty.reg Update .cache/wal/colors-retroarch.cfg Update .cache/wal/colors-rgb Update .cache/wal/colors-rio.toml Update .cache/wal/colors-rofi-dark.rasi Update .cache/wal/colors-rofi-light.rasi Update .cache/wal/colors-speedcrunch.json Update .cache/wal/colors-sway Update .cache/wal/colors-themer.js Update .cache/wal/colors-tilix.json Update .cache/wal/colors-tofi Update .cache/wal/colors-tty.sh Update .cache/wal/colors-vscode.json Update .cache/wal/colors-wal-dmenu.h Update .cache/wal/colors-wal-dwl.h Update .cache/wal/colors-wal-dwm-urg.h Update .cache/wal/colors-wal-dwm.h Update .cache/wal/colors-wal-st.h Update .cache/wal/colors-wal-tabbed.h Update .cache/wal/colors-wal.vim Update .cache/wal/colors-waybar.css Update .cache/wal/colors-zathura Update .cache/wal/colors-zed.json Update .cache/wal/colors.Xresources Update .cache/wal/colors.css Update .cache/wal/colors.fish Update .cache/wal/colors.json Update .cache/wal/colors.properties Update .cache/wal/colors.scss Update .cache/wal/colors.sh Update .cache/wal/colors.styl Update .cache/wal/colors.yml Update .cache/wal/ghostty.conf Add .cache/wal/schemes/_home_bjtn__config_wallpapers_182991-2560x1440-desktop-hd-demon-slayer-kimetsu-no-yaiba-background-image_jpg_dark_None_None_831480_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_2jzlLHXn_o_jpg_dark_None_None_2762630_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_34_png_dark_None_None_12183049_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_37ebc44b4782d15ceccbbca8032d4d8c_jpg_dark_None_None_341527_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_aesthetic-mountain-lake-desktop-wallpaper_jpg_dark_None_None_4020937_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_bleach-rebirth-of-souls-game-hd-wallpaper-uhdpaper_com-172@5@e_jpg_dark_None_None_543425_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_cyberpunk-edgerunners-lucy-and-david-in-moon-4k-wallpaper-uhdpaper_com-628@1@i_jpg_dark_None_None_1813186_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_hollow-knight-2_jpg_dark_None_None_524969_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_landscape-girl-under-cherry-blossoms-desktop-wallpaper_jpg_dark_None_None_4884110_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_metroid1_jpg_dark_None_None_407045_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_moon-purple-clouds-space-desktop-wallpaper_jpg_dark_None_None_3902927_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_wallhaven-vqy2jp_3840x2160_png_dark_None_None_8535515_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_whisper_of_the_heart_by_gydw1n_ddn6c37_png_dark_None_None_2445493_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_zelda-tearskingdom-mastersword-02-1920x1080-1_jpg_dark_None_None_442138_2.0.0.json Add .cache/wal/schemes/_home_bjtn__config_wallpapers_zeraora-pokemon-hd-wallpaper-uhdpaper_com-467@1@j_jpg_dark_None_None_748416_2.0.0.json Update .cache/wal/sequences Update .cache/wal/wal

--- dot_cache/wal/colors-tty.sh
+++ dot_cache/wal/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
     printf '%b' '\e]P0000000
-                 \e]P1696053
-                 \e]P2915C39
-                 \e]P38E6C56
-                 \e]P4A2926B
-                 \e]P5D0AB6B
-                 \e]P65C7583
-                 \e]P7c6c7c2
-                 \e]P86b715d
-                 \e]P9696053
-                 \e]PA915C39
-                 \e]PB8E6C56
-                 \e]PCA2926B
-                 \e]PDD0AB6B
-                 \e]PE5C7583
-                 \e]PFc6c7c2
+                 \e]P19C4C6D
+                 \e]P2B8807A
+                 \e]P359708F
+                 \e]P4AA678A
+                 \e]P5E16C90
+                 \e]P67092AC
+                 \e]P7c4c2c7
+                 \e]P8655c71
+                 \e]P99C4C6D
+                 \e]PAB8807A
+                 \e]PB59708F
+                 \e]PCAA678A
+                 \e]PDE16C90
+                 \e]PE7092AC
+                 \e]PFc4c2c7
                  \ec'
--- dot_cache/wal/colors-wal-dwm.h
+++ dot_cache/wal/colors-wal-dwm.h
@@ -1,10 +1,10 @@
-static const char norm_fg[] = "#c6c7c2";
+static const char norm_fg[] = "#c4c2c7";
 static const char norm_bg[] = "#000000";
-static const char norm_border[] = "#6b715d";
+static const char norm_border[] = "#655c71";
 
-static const char sel_fg[] = "#c6c7c2";
-static const char sel_bg[] = "#696053";
-static const char sel_border[] = "#c6c7c2";
+static const char sel_fg[] = "#c4c2c7";
+static const char sel_bg[] = "#9C4C6D";
+static const char sel_border[] = "#c4c2c7";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
