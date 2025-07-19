e1e4b129519488c1b0fb4c222bf5b5800d6ab274
bjtn <github@bjtn.me>
2025-07-18 20:29:13 -0400

    Update .cache/wal/Colors.hs Update .cache/wal/colors Update .cache/wal/colors--nodim-alacritty.yml Update .cache/wal/colors-alacritty.toml Update .cache/wal/colors-alacritty.yml Update .cache/wal/colors-foot.ini Update .cache/wal/colors-fuzzel.ini Update .cache/wal/colors-hyprland.conf Update .cache/wal/colors-kitty.conf Update .cache/wal/colors-konsole.colorscheme Update .cache/wal/colors-leftwm-theme.ron Update .cache/wal/colors-mako Update .cache/wal/colors-nqq.css Update .cache/wal/colors-oomox Update .cache/wal/colors-polybar Update .cache/wal/colors-putty.reg Update .cache/wal/colors-retroarch.cfg Update .cache/wal/colors-rgb Update .cache/wal/colors-rio.toml Update .cache/wal/colors-rofi-dark.rasi Update .cache/wal/colors-rofi-light.rasi Update .cache/wal/colors-speedcrunch.json Update .cache/wal/colors-sway Update .cache/wal/colors-themer.js Update .cache/wal/colors-tilix.json Update .cache/wal/colors-tofi Update .cache/wal/colors-tty.sh Update .cache/wal/colors-vscode.json Update .cache/wal/colors-wal-dmenu.h Update .cache/wal/colors-wal-dwl.h Update .cache/wal/colors-wal-dwm-urg.h Update .cache/wal/colors-wal-dwm.h Update .cache/wal/colors-wal-st.h Update .cache/wal/colors-wal-tabbed.h Update .cache/wal/colors-wal.vim Update .cache/wal/colors-waybar.css Update .cache/wal/colors-zathura Update .cache/wal/colors-zed.json Update .cache/wal/colors.Xresources Update .cache/wal/colors.css Update .cache/wal/colors.fish Update .cache/wal/colors.json Update .cache/wal/colors.properties Update .cache/wal/colors.scss Update .cache/wal/colors.sh Update .cache/wal/colors.styl Update .cache/wal/colors.yml Update .cache/wal/ghostty.conf Update .cache/wal/last_used_theme Update .cache/wal/sequences Update .cache/wal/wal

--- dot_cache/wal/colors-tty.sh
+++ dot_cache/wal/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
     printf '%b' '\e]P0000000
-                 \e]P1BB4446
-                 \e]P2EC665B
-                 \e]P3AB8975
-                 \e]P4F9946D
-                 \e]P5F75B8F
-                 \e]P6F4A897
-                 \e]P7c3c1c1
-                 \e]P86a5a57
-                 \e]P9BB4446
-                 \e]PAEC665B
-                 \e]PBAB8975
-                 \e]PCF9946D
-                 \e]PDF75B8F
-                 \e]PEF4A897
-                 \e]PFc3c1c1
+                 \e]P15D92C3
+                 \e]P2AE86A7
+                 \e]P3E0929D
+                 \e]P4FDD5AC
+                 \e]P5A4B9D5
+                 \e]P6DAD8DC
+                 \e]P7c5c5ca
+                 \e]P8656379
+                 \e]P95D92C3
+                 \e]PAAE86A7
+                 \e]PBE0929D
+                 \e]PCFDD5AC
+                 \e]PDA4B9D5
+                 \e]PEDAD8DC
+                 \e]PFc5c5ca
                  \ec'
