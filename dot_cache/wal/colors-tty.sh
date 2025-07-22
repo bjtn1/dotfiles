f5a81878d8d47888579cc0c9d18c169f2a239089
bjtn <github@bjtn.me>
2025-07-21 22:13:11 -0400

    Update .cache/wal/Colors.hs Update .cache/wal/colors Update .cache/wal/colors--nodim-alacritty.yml Update .cache/wal/colors-alacritty.toml Update .cache/wal/colors-alacritty.yml Update .cache/wal/colors-foot.ini Update .cache/wal/colors-fuzzel.ini Update .cache/wal/colors-hyprland.conf Update .cache/wal/colors-kitty.conf Update .cache/wal/colors-konsole.colorscheme Update .cache/wal/colors-leftwm-theme.ron Update .cache/wal/colors-mako Update .cache/wal/colors-nqq.css Update .cache/wal/colors-oomox Update .cache/wal/colors-polybar Update .cache/wal/colors-putty.reg Update .cache/wal/colors-retroarch.cfg Update .cache/wal/colors-rgb Update .cache/wal/colors-rio.toml Update .cache/wal/colors-rofi-dark.rasi Update .cache/wal/colors-rofi-light.rasi Update .cache/wal/colors-speedcrunch.json Update .cache/wal/colors-sway Update .cache/wal/colors-themer.js Update .cache/wal/colors-tilix.json Update .cache/wal/colors-tofi Update .cache/wal/colors-tty.sh Update .cache/wal/colors-vscode.json Update .cache/wal/colors-wal-dmenu.h Update .cache/wal/colors-wal-dwl.h Update .cache/wal/colors-wal-dwm-urg.h Update .cache/wal/colors-wal-dwm.h Update .cache/wal/colors-wal-st.h Update .cache/wal/colors-wal-tabbed.h Update .cache/wal/colors-wal.vim Update .cache/wal/colors-waybar.css Update .cache/wal/colors-zathura Update .cache/wal/colors-zed.json Update .cache/wal/colors.Xresources Update .cache/wal/colors.css Update .cache/wal/colors.fish Update .cache/wal/colors.json Update .cache/wal/colors.properties Update .cache/wal/colors.scss Update .cache/wal/colors.sh Update .cache/wal/colors.styl Update .cache/wal/colors.yml Update .cache/wal/ghostty.conf Update .cache/wal/last_used_theme Update .cache/wal/sequences Update .cache/wal/wal

--- dot_cache/wal/colors-tty.sh
+++ dot_cache/wal/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
     printf '%b' '\e]P0000000
-                 \e]P1D96829
-                 \e]P2AB7251
-                 \e]P3DB9B58
-                 \e]P4EBB65B
-                 \e]P5987F80
-                 \e]P6C7A99F
-                 \e]P7c6c3c2
-                 \e]P870615b
-                 \e]P9D96829
-                 \e]PAAB7251
-                 \e]PBDB9B58
-                 \e]PCEBB65B
-                 \e]PD987F80
-                 \e]PEC7A99F
-                 \e]PFc6c3c2
+                 \e]P1334853
+                 \e]P23B5364
+                 \e]P3565E5F
+                 \e]P48E725B
+                 \e]P5A1906C
+                 \e]P66A7C81
+                 \e]P7c2c4c7
+                 \e]P85c6571
+                 \e]P9334853
+                 \e]PA3B5364
+                 \e]PB565E5F
+                 \e]PC8E725B
+                 \e]PDA1906C
+                 \e]PE6A7C81
+                 \e]PFc2c4c7
                  \ec'
--- dot_cache/wal/colors-wal-dmenu.h
+++ dot_cache/wal/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c6c3c2", "#000000" },
-	[SchemeSel] = { "#c6c3c2", "#D96829" },
-	[SchemeOut] = { "#c6c3c2", "#C7A99F" },
+	[SchemeNorm] = { "#c2c4c7", "#000000" },
+	[SchemeSel] = { "#c2c4c7", "#334853" },
+	[SchemeOut] = { "#c2c4c7", "#6A7C81" },
 };
