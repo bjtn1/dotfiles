d369d7ad9e84c534a71ce5a0c0e42fb0f52b0146
bjtn <github@bjtn.me>
2025-07-21 22:16:10 -0400

    Update .cache/wal/Colors.hs Update .cache/wal/colors Update .cache/wal/colors--nodim-alacritty.yml Update .cache/wal/colors-alacritty.toml Update .cache/wal/colors-alacritty.yml Update .cache/wal/colors-foot.ini Update .cache/wal/colors-fuzzel.ini Update .cache/wal/colors-hyprland.conf Update .cache/wal/colors-kitty.conf Update .cache/wal/colors-konsole.colorscheme Update .cache/wal/colors-leftwm-theme.ron Update .cache/wal/colors-mako Update .cache/wal/colors-nqq.css Update .cache/wal/colors-oomox Update .cache/wal/colors-polybar Update .cache/wal/colors-putty.reg Update .cache/wal/colors-retroarch.cfg Update .cache/wal/colors-rgb Update .cache/wal/colors-rio.toml Update .cache/wal/colors-rofi-dark.rasi Update .cache/wal/colors-rofi-light.rasi Update .cache/wal/colors-speedcrunch.json Update .cache/wal/colors-sway Update .cache/wal/colors-themer.js Update .cache/wal/colors-tilix.json Update .cache/wal/colors-tofi Update .cache/wal/colors-tty.sh Update .cache/wal/colors-vscode.json Update .cache/wal/colors-wal-dmenu.h Update .cache/wal/colors-wal-dwl.h Update .cache/wal/colors-wal-dwm-urg.h Update .cache/wal/colors-wal-dwm.h Update .cache/wal/colors-wal-st.h Update .cache/wal/colors-wal-tabbed.h Update .cache/wal/colors-wal.vim Update .cache/wal/colors-waybar.css Update .cache/wal/colors-zathura Update .cache/wal/colors-zed.json Update .cache/wal/colors.Xresources Update .cache/wal/colors.css Update .cache/wal/colors.fish Update .cache/wal/colors.json Update .cache/wal/colors.properties Update .cache/wal/colors.scss Update .cache/wal/colors.sh Update .cache/wal/colors.styl Update .cache/wal/colors.yml Update .cache/wal/ghostty.conf Update .cache/wal/last_used_theme Update .cache/wal/sequences Update .cache/wal/wal

--- dot_cache/wal/colors-wal-dwm-urg.h
+++ dot_cache/wal/colors-wal-dwm-urg.h
@@ -1,14 +1,14 @@
-static const char norm_fg[] = "#c2c1c4";
+static const char norm_fg[] = "#c8c9d2";
 static const char norm_bg[] = "#000000";
-static const char norm_border[] = "#5b596c";
+static const char norm_border[] = "#707389";
 
-static const char sel_fg[] = "#c2c1c4";
-static const char sel_bg[] = "#EA5731";
-static const char sel_border[] = "#c2c1c4";
+static const char sel_fg[] = "#c8c9d2";
+static const char sel_bg[] = "#6693CE";
+static const char sel_border[] = "#c8c9d2";
 
-static const char urg_fg[] = "#c2c1c4";
-static const char urg_bg[] = "#D23B3A";
-static const char urg_border[] = "#D23B3A";
+static const char urg_fg[] = "#c8c9d2";
+static const char urg_bg[] = "#3C82CE";
+static const char urg_border[] = "#3C82CE";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
