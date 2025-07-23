87523291116f4dae5a1d63eb71458aa5214d750b
bjtn <github@bjtn.me>
2025-07-22 22:24:30 -0400

    Update .cache/wal/Colors.hs Update .cache/wal/colors Update .cache/wal/colors--nodim-alacritty.yml Update .cache/wal/colors-alacritty.toml Update .cache/wal/colors-alacritty.yml Update .cache/wal/colors-foot.ini Update .cache/wal/colors-fuzzel.ini Update .cache/wal/colors-hyprland.conf Update .cache/wal/colors-kitty.conf Update .cache/wal/colors-konsole.colorscheme Update .cache/wal/colors-leftwm-theme.ron Update .cache/wal/colors-mako Update .cache/wal/colors-nqq.css Update .cache/wal/colors-oomox Update .cache/wal/colors-polybar Update .cache/wal/colors-putty.reg Update .cache/wal/colors-retroarch.cfg Update .cache/wal/colors-rgb Update .cache/wal/colors-rio.toml Update .cache/wal/colors-rofi-dark.rasi Update .cache/wal/colors-rofi-light.rasi Update .cache/wal/colors-speedcrunch.json Update .cache/wal/colors-sway Update .cache/wal/colors-themer.js Update .cache/wal/colors-tilix.json Update .cache/wal/colors-tofi Update .cache/wal/colors-tty.sh Update .cache/wal/colors-vscode.json Update .cache/wal/colors-wal-dmenu.h Update .cache/wal/colors-wal-dwl.h Update .cache/wal/colors-wal-dwm-urg.h Update .cache/wal/colors-wal-dwm.h Update .cache/wal/colors-wal-st.h Update .cache/wal/colors-wal-tabbed.h Update .cache/wal/colors-wal.vim Update .cache/wal/colors-waybar.css Update .cache/wal/colors-zathura Update .cache/wal/colors-zed.json Update .cache/wal/colors.Xresources Update .cache/wal/colors.css Update .cache/wal/colors.fish Update .cache/wal/colors.json Update .cache/wal/colors.properties Update .cache/wal/colors.scss Update .cache/wal/colors.sh Update .cache/wal/colors.styl Update .cache/wal/colors.yml Update .cache/wal/ghostty.conf Update .cache/wal/last_used_theme Update .cache/wal/sequences Update .cache/wal/wal

--- dot_cache/wal/colors-wal-st.h
+++ dot_cache/wal/colors-wal-st.h
@@ -2,28 +2,28 @@ const char *colorname[] = {
 
   /* 8 normal colors */
   [0] = "#000000", /* black   */
-  [1] = "#B38478", /* red     */
-  [2] = "#DE9574", /* green   */
-  [3] = "#FDC578", /* yellow  */
-  [4] = "#997F81", /* blue    */
-  [5] = "#B79489", /* magenta */
-  [6] = "#DFAB90", /* cyan    */
-  [7] = "#c4c3c4", /* white   */
+  [1] = "#344871", /* red     */
+  [2] = "#4F516E", /* green   */
+  [3] = "#37518C", /* yellow  */
+  [4] = "#4B55A0", /* blue    */
+  [5] = "#5468A0", /* magenta */
+  [6] = "#906983", /* cyan    */
+  [7] = "#c2c3c5", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#6e5a6e",  /* black   */
-  [9]  = "#B38478",  /* red     */
-  [10] = "#DE9574", /* green   */
-  [11] = "#FDC578", /* yellow  */
-  [12] = "#997F81", /* blue    */
-  [13] = "#B79489", /* magenta */
-  [14] = "#DFAB90", /* cyan    */
-  [15] = "#c4c3c4", /* white   */
+  [8]  = "#5a636e",  /* black   */
+  [9]  = "#344871",  /* red     */
+  [10] = "#4F516E", /* green   */
+  [11] = "#37518C", /* yellow  */
+  [12] = "#4B55A0", /* blue    */
+  [13] = "#5468A0", /* magenta */
+  [14] = "#906983", /* cyan    */
+  [15] = "#c2c3c5", /* white   */
 
   /* special colors */
   [256] = "#000000", /* background */
-  [257] = "#c4c3c4", /* foreground */
-  [258] = "#c4c3c4",     /* cursor */
+  [257] = "#c2c3c5", /* foreground */
+  [258] = "#c2c3c5",     /* cursor */
 };
 
 /* Default colors (colorname index)
--- dot_cache/wal/colors-wal-tabbed.h
+++ dot_cache/wal/colors-wal-tabbed.h
@@ -1,6 +1,6 @@
 static const char* selbgcolor   = "#000000";
-static const char* selfgcolor   = "#c4c3c4";
-static const char* normbgcolor  = "#DE9574";
-static const char* normfgcolor  = "#c4c3c4";
-static const char* urgbgcolor   = "#B38478";
-static const char* urgfgcolor   = "#c4c3c4";
+static const char* selfgcolor   = "#c2c3c5";
+static const char* normbgcolor  = "#4F516E";
+static const char* normfgcolor  = "#c2c3c5";
+static const char* urgbgcolor   = "#344871";
+static const char* urgfgcolor   = "#c2c3c5";
