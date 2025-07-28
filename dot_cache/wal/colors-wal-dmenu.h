a141c5f4a6dd43ad4d62a2ffa89d2dbfe9332b89
bjtn <github@bjtn.me>
2025-07-28 17:30:49 -0400

    Update .cache/wal/Colors.hs Update .cache/wal/colors Update .cache/wal/colors--nodim-alacritty.yml Update .cache/wal/colors-alacritty.toml Update .cache/wal/colors-alacritty.yml Update .cache/wal/colors-foot.ini Update .cache/wal/colors-fuzzel.ini Update .cache/wal/colors-hyprland.conf Update .cache/wal/colors-kitty.conf Update .cache/wal/colors-konsole.colorscheme Update .cache/wal/colors-leftwm-theme.ron Update .cache/wal/colors-mako Update .cache/wal/colors-nqq.css Update .cache/wal/colors-oomox Update .cache/wal/colors-polybar Update .cache/wal/colors-putty.reg Update .cache/wal/colors-retroarch.cfg Update .cache/wal/colors-rgb Update .cache/wal/colors-rio.toml Update .cache/wal/colors-rofi-dark.rasi Update .cache/wal/colors-rofi-light.rasi Update .cache/wal/colors-speedcrunch.json Update .cache/wal/colors-sway Update .cache/wal/colors-themer.js Update .cache/wal/colors-tilix.json Update .cache/wal/colors-tofi Update .cache/wal/colors-tty.sh Update .cache/wal/colors-vscode.json Update .cache/wal/colors-wal-dmenu.h Update .cache/wal/colors-wal-dwl.h Update .cache/wal/colors-wal-dwm-urg.h Update .cache/wal/colors-wal-dwm.h Update .cache/wal/colors-wal-st.h Update .cache/wal/colors-wal-tabbed.h Update .cache/wal/colors-wal.vim Update .cache/wal/colors-waybar.css Update .cache/wal/colors-zathura Update .cache/wal/colors-zed.json Update .cache/wal/colors.Xresources Update .cache/wal/colors.css Update .cache/wal/colors.fish Update .cache/wal/colors.json Update .cache/wal/colors.properties Update .cache/wal/colors.scss Update .cache/wal/colors.sh Update .cache/wal/colors.styl Update .cache/wal/colors.yml Update .cache/wal/ghostty.conf Update .cache/wal/last_used_theme Update .cache/wal/sequences Update .cache/wal/wal

--- dot_cache/wal/colors-wal-dmenu.h
+++ dot_cache/wal/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cbcdd0", "#000000" },
-	[SchemeSel] = { "#cbcdd0", "#C1AAAF" },
-	[SchemeOut] = { "#cbcdd0", "#CED6E2" },
+	[SchemeNorm] = { "#c3c4c7", "#000000" },
+	[SchemeSel] = { "#c3c4c7", "#4C4C56" },
+	[SchemeOut] = { "#c3c4c7", "#556E90" },
 };
--- dot_cache/wal/colors-wal-tabbed.h
+++ dot_cache/wal/colors-wal-tabbed.h
@@ -1,6 +1,6 @@
 static const char* selbgcolor   = "#000000";
-static const char* selfgcolor   = "#cbcdd0";
-static const char* normbgcolor  = "#96B4CE";
-static const char* normfgcolor  = "#cbcdd0";
-static const char* urgbgcolor   = "#C1AAAF";
-static const char* urgfgcolor   = "#cbcdd0";
+static const char* selfgcolor   = "#c3c4c7";
+static const char* normbgcolor  = "#5C5D6A";
+static const char* normfgcolor  = "#c3c4c7";
+static const char* urgbgcolor   = "#4C4C56";
+static const char* urgfgcolor   = "#c3c4c7";
