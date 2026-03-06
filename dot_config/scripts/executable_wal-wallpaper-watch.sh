7e9c7235c021bf7b598ff1ecc1894141d77cd9c6
Brandon Jose Tenorio Noguera <github@bjtn.me>
2026-03-05 19:38:41 -0500

    Update .config/kitty/kitty.conf Update .config/scripts/wal-wallpaper-watch.sh

--- dot_config/scripts/executable_wal-wallpaper-watch.sh
+++ dot_config/scripts/executable_wal-wallpaper-watch.sh
@@ -8,7 +8,12 @@ while true; do
     # Force background and terminal black (color0) to stay pure black
     sed -i '' 's/^background[[:space:]].*/background         #000000/' ~/.config/kitty/current-theme.conf
     sed -i '' 's/^color0[[:space:]].*/color0       #000000/' ~/.config/kitty/current-theme.conf
-    /opt/homebrew/bin/kitty @ --to unix:/tmp/kitty.sock set-colors --all --configured ~/.config/kitty/current-theme.conf 2>/dev/null || kill -SIGUSR1 $(pgrep -x kitty) 2>/dev/null
+    KITTY_SOCK=$(ls /tmp/kitty.sock-* 2>/dev/null | head -1)
+    if [ -n "$KITTY_SOCK" ]; then
+      /opt/homebrew/bin/kitty @ --to unix:"$KITTY_SOCK" set-colors --all --configured ~/.config/kitty/current-theme.conf 2>/dev/null
+    else
+      kill -SIGUSR1 $(pgrep -x kitty) 2>/dev/null
+    fi
     LAST="$CURRENT"
   fi
   sleep 5
