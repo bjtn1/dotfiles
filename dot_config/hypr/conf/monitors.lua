f5b2ed67830ec200b3a064f4265efbc2ac154361
Brandon Jose Tenorio Noguera <github@bjtn.me>
2026-07-21 21:58:35 -0400

    Update .config/hypr/conf/monitors.lua

--- dot_config/hypr/conf/monitors.lua
+++ dot_config/hypr/conf/monitors.lua
@@ -1,3 +1,8 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 
+-- DP-2 on the left, DP-1 to its right
+hl.monitor({ output = "DP-2", mode = "preferred", position = "0x0", scale = 1 })
+hl.monitor({ output = "DP-1", mode = "preferred", position = "auto", scale = 1 })
+
+-- fallback for any other monitor plugged in
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
