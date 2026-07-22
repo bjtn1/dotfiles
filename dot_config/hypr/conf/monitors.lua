6b0f1b22a4dba9aaebab978487f4234b3f71826e
Brandon J. T. Noguera <github@bjtn.me>
2026-07-21 22:07:35 -0400

    Update .config/fish/fish_variables Update .config/hypr/conf/monitors.lua Update .config/hypr/current_wallpaper Update .config/waypaper/config.ini

--- dot_config/hypr/conf/monitors.lua
+++ dot_config/hypr/conf/monitors.lua
@@ -1,3 +1,3 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
+hl.monitor({ output = "DP-2", mode = "preferred", position = "auto", scale = "auto" })
