9e55338763f39fd7ffe348b40fb918a94bd49ed9
Brandon J. T. Noguera <github@bjtn.me>
2026-07-28 18:48:32 -0400

    Add .config/hypr/conf/workspacerules.lua

---
+++ dot_config/hypr/conf/workspacerules.lua
@@ -0,0 +1,16 @@
+hl.workspace_rule({
+  workspace = "1",
+  monitor = "DP-2"
+})
+
+hl.workspace_rule({
+  workspace = "2",
+  monitor = "DP-1"
+})
+
+hl.workspace_rule({
+  workspace = "3",
+  monitor = "HDMI-A-1"
+})
+
+
