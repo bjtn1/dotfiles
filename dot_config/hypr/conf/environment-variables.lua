f0abe7d04d1eacc2964455026c3d8a9e0b5892cb
Brandon J. T. Noguera <github@bjtn.me>
2026-07-27 22:20:48 -0400

    Update .config/hypr/conf/environment-variables.lua

--- dot_config/hypr/conf/environment-variables.lua
+++ dot_config/hypr/conf/environment-variables.lua
@@ -2,3 +2,7 @@
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+-- Qt apps (dolphin, etc.): read kdeglobals for palette, wal-themed via
+-- wal/templates/colors-kdeglobals.sh
+hl.env("QT_QPA_PLATFORMTHEME", "kde")
