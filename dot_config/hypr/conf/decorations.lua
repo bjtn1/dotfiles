223eae8c643999875a656cdc0f7feb74a1145391
Brandon J. T. Noguera <github@bjtn.me>
2026-07-28 21:45:27 -0400

    Update .config/hypr/conf/decorations.lua Update .config/hypr/conf/windowrules.lua

--- dot_config/hypr/conf/decorations.lua
+++ dot_config/hypr/conf/decorations.lua
@@ -7,7 +7,7 @@ hl.config({
   decoration = {
     rounding = 10,
     active_opacity = 1.0,
-    inactive_opacity = 1.0,
+    inactive_opacity = 0.70,
     fullscreen_opacity = 1.0,
     blur = {
       enabled = false,
