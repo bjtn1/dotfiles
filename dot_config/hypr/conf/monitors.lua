e491710fd4a3bf1992fbdc361241aa030fd7be5b
Brandon J. T. Noguera <github@bjtn.me>
2026-08-24 16:50:35 -0400

    Update .config/hypr/conf/monitors.lua Update .config/hypr/conf/workspacerules.lua

--- dot_config/hypr/conf/monitors.lua
+++ dot_config/hypr/conf/monitors.lua
@@ -1,8 +1,8 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 
 -- Left to right: DP-2 (Asus 200Hz), DP-1 (Dell 60Hz), HDMI-A-1 (TV)
-hl.monitor({ output = "DP-2", mode = "preferred", position = "0x0", scale = 1 })
-hl.monitor({ output = "DP-1", mode = "preferred", position = "1920x0", scale = 1 })
+hl.monitor({ output = "DP-1", mode = "preferred", position = "0x0", scale = 1 })
+hl.monitor({ output = "DP-2", mode = "preferred", position = "1920x0", scale = 1 })
 
 -- TV: EDID reports a tiny physical size, so Hyprland's "auto" scale heuristic
 -- was guessing 2x (blurry upscaled 1080p) -- pin it to native 1:1 scale.
--- dot_config/hypr/conf/workspacerules.lua
+++ dot_config/hypr/conf/workspacerules.lua
@@ -1,11 +1,11 @@
 hl.workspace_rule({
   workspace = "1",
-  monitor = "DP-2"
+  monitor = "DP-1"
 })
 
 hl.workspace_rule({
   workspace = "2",
-  monitor = "DP-1"
+  monitor = "DP-2"
 })
 
 hl.workspace_rule({
